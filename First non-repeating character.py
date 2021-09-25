31811bf442dd421b3cdb0fd9098cc14f6eac3547
Mohammad ward <72629614+md-ward@users.noreply.github.com>
2021-09-25 11:58:02 +0300

    Add files via upload

---
+++ First non-repeating character.py
@@ -0,0 +1,8 @@
+from collections import Counter
+def first_non_repeating_letter(string):
+    cnt = Counter(string.lower())
+    for letter in string:
+        if cnt[letter.lower()] == 1:
+            return letter
+    return ''
+print(first_non_repeating_letter("sTreSS"))
